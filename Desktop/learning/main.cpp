9a14cff72a2e8528e3f6ba826b39a945dae1d42d
devanshi <devanshi.katyal09@gmail.com>
2020-02-24 17:07:48 +0530

    hello

--- Desktop/learning/main.cpp
+++ Desktop/learning/main.cpp
@@ -5,5 +5,7 @@ using namespace std;
 int main()
 {
     cout << "Hello world! this is devanshi katyal right here!!" << endl;
+    cout<<" this is me in the whole world"<<endl;
+    cout<<" this is not going to be when we shift"<<endl;
     return 0;
 }
